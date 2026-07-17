9224d8a01eb0cbd16ad24ed62f4315e67d5ce972
Ziadd_Zamel <ziaddmahmoude@gmail.com>
2026-07-17 04:28:38 -0700

    clean projct filles

--- src/components/skeletons/category/category-item.skeleton.tsx
+++
@@ -1,3 +0,0 @@
-export default function CategoryItemSkeleton() {
-  return <div>CategoryItemSkeleton Demo</div>;
-}
--- src/lib/constants/api.constant.ts
+++
@@ -1,3 +0,0 @@
-export const JSON_HEADER = {
-  "Content-Type": "application/json",
-};
